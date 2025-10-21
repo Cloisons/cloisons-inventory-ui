979fc38cca3b116e71b648a8163ba99de684b5b4
sarath chandran <sarathchandran12370@gmail.com>
2025-10-21 22:06:15 +0530

    api fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.cloisons.com',
+  apiUrl: 'https://api.thecloisons.com',
   appName: 'Cloisons Inventory Management',
   version: '1.0.0',
   enableLogging: false,
